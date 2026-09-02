e542e69ce1ba1e12a5f362cd52fdc13f21c4fd1d
AgZ47 <abyaldrin@gmail.com>
2026-09-02 21:29:31 +0530

    Transaction Component Basic functional implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import { Transaction } from "./Components/Transaction";
 function App() {
   return (
     <section>
-      <div>
-        <Transaction />
+      <div className="bg-sky-900 h-screen p-2">
+        <div></div>
+        <div>
+          <Transaction
+            id={1}
+            amount={500}
+            category="expense"
+            date={new Date("2026-09-02T12:00:00Z")}
+            desc="pari"
+          />
+        </div>
       </div>
     </section>
   );
--- src/Components/Transaction.tsx
+++ src/Components/Transaction.tsx
@@ -1,7 +1,23 @@
-export const Transaction = () => {
+type transactionProps = {
+  id: number;
+  amount: number;
+  category: "income" | "expense";
+  date: Date;
+  desc: string;
+};
+
+export const Transaction = (props: transactionProps) => {
   return (
-    <div>
-      <h2>Transaction</h2>
+    <div className="bg-sky-300 rounded-xl border-3 border-sky-500 text-black p-2">
+      <div className="flex flex-row gap-3">
+        <h2>
+          {props.amount}
+          {props.category == "income" ? "+" : "-"}
+        </h2>
+        <h2>{props.category}</h2>
+        <h2>{props.date.toDateString()}</h2>
+      </div>
+      <p>{props.desc}</p>
     </div>
   );
 };
